ff82b53aa2016f4281d69a0bad9714756a9d2d46
LEEYOSEPH <laksmi0628@gmail.com>
2022-08-28 08:13:42 +0900

    feat : MockMvc로 테스트 코드 작성

---
+++ src/main/java/com/gesipan/api/board/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.gesipan.api.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @GetMapping("/v1/posts")
+    public String get() {
+        return "hello World";
+    }
+}
